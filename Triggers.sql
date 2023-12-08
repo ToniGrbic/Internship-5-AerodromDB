e67dab2ff0776754294a1a4f03faefe46977e834
ToniGrbic <tonigrbic.5@gmail.com>
2023-12-08 20:09:21 +0100

    added trigger for checking valid loyalty card

--- Triggers.sql
+++ Triggers.sql
@@ -1,3 +1,4 @@
+<!--Trigger to check if the flight capacity is greater than the max passenger capacity of the plane-->
 CREATE OR REPLACE FUNCTION check_flight_capacity()
     RETURNS TRIGGER AS $$
 BEGIN
@@ -13,3 +14,30 @@ CREATE TRIGGER enforce_flight_capacity
     ON Flights
     FOR EACH ROW
     EXECUTE FUNCTION check_flight_capacity();
+
+
+<!--Trigger to check if the loyalty card is valid-->
+CREATE OR REPLACE FUNCTION check_loyalty_card_function()
+RETURNS TRIGGER AS $$
+BEGIN
+    IF NEW.LoyaltyCardExpiryDate IS NULL AND (
+        SELECT COUNT(*) FROM tickets WHERE UserID = NEW.UserID
+    ) < 10 THEN
+        RAISE EXCEPTION 'Loyalty card requires at least 10 purchased tickets.';
+    END IF;
+
+    IF NEW.LoyaltyCardExpiryDate IS NOT NULL AND (
+        SELECT COUNT(*) FROM tickets WHERE UserID = NEW.UserID
+    ) < 10 AND NEW.LoyaltyCardExpiryDate > CURRENT_DATE THEN
+        RAISE EXCEPTION 'Loyalty card requires at least 10 purchased tickets and must expire in the future.';
+    END IF;
+
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER check_loyalty_card_trigger
+BEFORE INSERT OR UPDATE
+ON Users
+FOR EACH ROW
+EXECUTE FUNCTION check_loyalty_card_function();
