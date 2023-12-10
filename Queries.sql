c6b85cdbb18acad332f0000c72cc5e97e4ce93b8
ToniGrbic <tonigrbic.5@gmail.com>
2023-12-10 03:11:47 +0100

    first couple of queries

--- Queries.sql
+++ Queries.sql
@@ -1,4 +1,14 @@
-<!-- ispis imena i modela svih aviona s kapacitetom većim od 100 -->
+-- ispis imena i modela svih aviona s kapacitetom većim od 100 
 SELECT Name, Model FROM Planes
 WHERE MaxPassangerCapacity > 100;
+
+-- ispis svih karata čija je cijena između 100 i 200 eura
+SELECT * FROM Tickets
+WHERE TicketPrice BETWEEN 100 AND 200;
+
+-- ispis svih pilotkinja s više od 20 odrađenih letova do danas
+SELECT Name, COUNT(*) as FlightsNumber  FROM FlightPersonnel fp
+WHERE fp.Gender = 'Female' && FlightsNumber > 1;
+
+
 
